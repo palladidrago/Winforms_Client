169dbbb502630ddc369e757d4c316f50af3386d3
palladidrago <38191497+palladidrago@users.noreply.github.com>
2021-10-01 09:34:28 +0300

    Add files via upload

---
+++ WindownsFormsApp_Client/WindownsFormsApp_Client/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace WindownsFormsApp_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fNameLabel = new System.Windows.Forms.Label();
+            this.lNameLabel = new System.Windows.Forms.Label();
+            this.btcAmountLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fNameLabel
+            // 
+            this.fNameLabel.AutoSize = true;
+            this.fNameLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fNameLabel.Location = new System.Drawing.Point(30, 45);
+            this.fNameLabel.Name = "fNameLabel";
+            this.fNameLabel.Size = new System.Drawing.Size(157, 38);
+            this.fNameLabel.TabIndex = 0;
+            this.fNameLabel.Text = "First Name:";
+            // 
+            // lNameLabel
+            // 
+            this.lNameLabel.AutoSize = true;
+            this.lNameLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lNameLabel.Location = new System.Drawing.Point(30, 83);
+            this.lNameLabel.Name = "lNameLabel";
+            this.lNameLabel.Size = new System.Drawing.Size(153, 38);
+            this.lNameLabel.TabIndex = 0;
+            this.lNameLabel.Text = "Last Name:";
+            // 
+            // btcAmountLabel
+            // 
+            this.btcAmountLabel.AutoSize = true;
+            this.btcAmountLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btcAmountLabel.Location = new System.Drawing.Point(30, 121);
+            this.btcAmountLabel.Name = "btcAmountLabel";
+            this.btcAmountLabel.Size = new System.Drawing.Size(247, 38);
+            this.btcAmountLabel.TabIndex = 0;
+            this.btcAmountLabel.Text = "Amount of bitcoin:";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ageLabel.Location = new System.Drawing.Point(30, 159);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(72, 38);
+            this.ageLabel.TabIndex = 0;
+            this.ageLabel.Text = "Age:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(30, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(30, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Phone Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.btcAmountLabel);
+            this.Controls.Add(this.lNameLabel);
+            this.Controls.Add(this.fNameLabel);
+            this.Name = "Form1";
+            this.Text = "Amount of bitcoin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fNameLabel;
+        private System.Windows.Forms.Label lNameLabel;
+        private System.Windows.Forms.Label btcAmountLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindownsFormsApp_Client/WindownsFormsApp_Client/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindownsFormsApp_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+    public class Human
+    {
+        string fName { get; set; } string lName { get; set; } // Name of the guy
+        int amountOfBtc { get; set; } //Amount of bitcoinz
+        int age{ get; set; } //Age
+        string phoneNumber { get; set; } //Phone number
+        string bitcoinAddress { get; set; } //His bitcoin address
+
+        public Human(string fName, string lName, int amountOfBtc, int age, string phoneNumber, string bitcoinAddress)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.amountOfBtc = amountOfBtc;
+            this.age = age;
+            this.phoneNumber = phoneNumber;
+            this.bitcoinAddress = bitcoinAddress;
+        }
+    }
+}
